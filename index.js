b2ce2a02190b4c143409c4f6206649062dd2f2a2
TheBrainy06 <65811729+TheBrainy06@users.noreply.github.com>
2021-04-09 11:58:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+var apiKeys = {
+    i89eNOFdoIWHnFOfIW923jfnFHW: "Brainy",
+    niABIbidjboDbobo3iiBDibfoi3:"Agent",
+    jeijnUHEUb9BdnbidObnonoQond:"Neil",
+    uygsbFIB98hOFjnokNFnfoknjnF:"Sammy"
+}
+
+app.use(express.json())
+
+app.listen(
+    PORT,
+    () => console.log("API is listening!")
+)
+
+app.get("/API/check",(req,res) =>{
+    res.status(200).send({
+        status: "200",
+        working: true
+    })
+});
+
+app.post('/API/:apikey/:frndname',(req,res)=>{
+    const { frndname } = req.params;
+    const { apikey } = req.params;
+    console.log(req.params)
+    if (!apiKeys[apikey]){
+        res.status(418).send({
+            status: "Invalid API"
+        })
+    }
+
+    res.status(200).send({
+        reqType: "POST",
+        friendname: `${frndname}`
+    })
+});
